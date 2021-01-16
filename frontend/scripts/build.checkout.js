85916e8e32c96aba90dfe2aa50bf674b7a9bf580
Sacha Vogel <sacha_vogel@sluz.ch>
2021-01-16 14:48:02 +0100

    Create validation for checkout 2

--- frontend/scripts/build.checkout.js
+++ frontend/scripts/build.checkout.js
@@ -1,7 +1,38 @@
 const localhostUrl = "http://localhost:8000/";
-export async function checkoutPurchase() {
-    await fetch(localhostUrl + `cart/removeAll`, {
-        method: 'POST'
+async function getCart() {
+    let response = await fetch(localhostUrl + 'cart', {
+        method: 'GET'
     });
+    return await response.json();
+}
+async function updateCart() {
+    let response = await fetch(localhostUrl + 'cart/total', {
+        method: 'GET'
+    });
+    document.getElementById("total").innerHTML = await response.json();
+}
+const localhostUrl1 = "http://localhost:8000/";
+export async function checkoutPurchase(event) {
+    event.preventDefault();
+    let prename = document.getElementById("firstname").value;
+    let lastname = document.getElementById("lastname").value;
+    let email = document.getElementById("email").value;
+    let user = {
+        prename,
+        lastname,
+        email
+    };
+    let response = await fetch(localhostUrl1 + `cart/checkout`, {
+        method: 'POST',
+        body: JSON.stringify(user)
+    });
+    await updateCart();
+    let message = await response.json();
+    if (message != "") {
+        alert(message);
+    } else {
+        alert("Einkauf wurde erfolgreich abgeschlossen!");
+        window.location.href = "http://localhost:8000/";
+    }
 }
 document.getElementById("checkout-form").addEventListener("submit", checkoutPurchase);
--- frontend/scripts/checkout.ts
+++ frontend/scripts/checkout.ts
@@ -4,8 +4,25 @@ const localhostUrl = "http://localhost:8000/";
 
 document.getElementById("checkout-form").addEventListener("submit", checkoutPurchase);
 
-export async function checkoutPurchase() {
-    await fetch(localhostUrl + `cart/removeAll`, {
-        method: 'POST'
+export async function checkoutPurchase(event: any) { 
+    event.preventDefault();
+
+    let prename = document.getElementById("firstname").value;
+    let lastname = document.getElementById("lastname").value;
+    let email = document.getElementById("email").value;
+    let user = {prename, lastname, email};
+
+    let response = await fetch(localhostUrl + `cart/checkout`, {
+        method: 'POST',
+        body: JSON.stringify(user)
     });
+    await updateCart();
+
+    let message = await response.json();
+    if (message != "") {
+        alert(message);
+    } else {
+        alert("Einkauf wurde erfolgreich abgeschlossen!");
+        window.location.href = "http://localhost:8000/";
+    }
 }
